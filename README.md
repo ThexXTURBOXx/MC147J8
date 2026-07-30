45c29aa40af5607ccbdfb89d62d5859454e91b5d
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-30 16:57:26 +0200

    Fix <= 1.4.5 once again and remove BouncyCastle update

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Nilmod enabling Forge on Legacy Minecraft (1.3.2 up to 1.5.2) to load mods compi
 - Allow Java 8 in MCPC+
 - Exclude certain Java 9+ class files from loading (fixes crashes and errors)
 - Update to ASM 5.2 (from 4.0; provides proper Java 8 support)
-- Update BouncyCastle to 1.69 (from 1.47; fixes many vulnerabilities)
+<!-- Not anymore: - Update BouncyCastle to 1.69 (from 1.47; fixes many vulnerabilities) -->
 - Add Gson as an additional library (does not make anything worse, but may be nice to use for some legacy modders as a dependency that is now built-in)
 - Download libraries and dependencies from an actively working mirror
 - Increase hardcoded download limits
@@ -22,8 +22,8 @@ Nilmod enabling Forge on Legacy Minecraft (1.3.2 up to 1.5.2) to load mods compi
 **Q**: Is there a server version of this?  
 **A**: The mod file _should_ work just fine on both client and server.
 
-**Q**: What does this mod fix exactly?
+**Q**: What does this mod fix exactly?  
 **A**: If you need to ask this question, this Nilmod will probably not do anything useful for you. To summarise, there is additional setup necessary for ASM to load mods compiled with Java 8. Forge did not follow these additional setup steps and hence, it can only load mods compiled with Java ≤ 7. This Nilmod lets Forge apply these additional setup steps and allows Forge to load such mods.
 
 **Q**: Are there any plans for [any other Minecraft version]?  
-**A**: This mod may work just fine for other MC versions as well. If not: let me know and I will provide a proper port!
+**A**: This mod may work just fine for other MC versions as well. If not: let me know and I will try to provide a proper port!
--- src/main/java/de/femtopedia/legacyforgej8/transformers/CoreFMLLibrariesTransformer.java
+++ src/main/java/de/femtopedia/legacyforgej8/transformers/CoreFMLLibrariesTransformer.java
@@ -45,7 +45,6 @@ public class CoreFMLLibrariesTransformer extends MiniTransformer {
                 "argo-2.25.jar",
                 "guava-12.0.1.jar",
                 "asm-all-5.2.jar",
-                "bcprov-jdk15on-1.69.jar",
                 "gson-2.14.0.jar"
         };
 
@@ -53,7 +52,24 @@ public class CoreFMLLibrariesTransformer extends MiniTransformer {
                 "bb672829fde76cb163004752b86b0484bd0a7f4b",
                 "b8e78b9af7bf45900e14c6f958486b6ca682195f",
                 "2ea49e08b876bbd33e0a7ce75c8f371d29e1f10a",
-                "91e1628251cf3ca90093ce9d0fe67e5b7dab3850",
+                "efc0e34ede4e3204eaefb84a00e55e8c86634382"
+        };
+
+        private static final String[] libraries147 = new String[]{
+                "argo-2.25.jar",
+                "guava-12.0.1.jar",
+                "asm-all-5.2.jar",
+                //"bcprov-jdk15on-1.69.jar",
+                "bcprov-jdk15on-147.jar",
+                "gson-2.14.0.jar"
+        };
+
+        private static final String[] checksums147 = new String[]{
+                "bb672829fde76cb163004752b86b0484bd0a7f4b",
+                "b8e78b9af7bf45900e14c6f958486b6ca682195f",
+                "2ea49e08b876bbd33e0a7ce75c8f371d29e1f10a",
+                //"91e1628251cf3ca90093ce9d0fe67e5b7dab3850",
+                "b6f5d9926b0afbde9f4dbe3db88c5247be7794bb",
                 "efc0e34ede4e3204eaefb84a00e55e8c86634382"
         };
 
@@ -61,7 +77,8 @@ public class CoreFMLLibrariesTransformer extends MiniTransformer {
                 "argo-small-3.2.jar",
                 "guava-14.0-rc3.jar",
                 "asm-all-5.2.jar",
-                "bcprov-jdk15on-1.69.jar",
+                //"bcprov-jdk15on-1.69.jar",
+                "bcprov-jdk15on-147.jar",
                 null, // Will get replaced in method patcher
                 "scala-library.jar",
                 "gson-2.14.0.jar"
@@ -71,16 +88,25 @@ public class CoreFMLLibrariesTransformer extends MiniTransformer {
                 "58912ea2858d168c50781f956fa5b59f0f7c6b51",
                 "931ae21fa8014c3ce686aaa621eae565fefb1a6a",
                 "2ea49e08b876bbd33e0a7ce75c8f371d29e1f10a",
-                "91e1628251cf3ca90093ce9d0fe67e5b7dab3850",
+                //"91e1628251cf3ca90093ce9d0fe67e5b7dab3850",
+                "b6f5d9926b0afbde9f4dbe3db88c5247be7794bb",
                 null, // Will get replaced in method patcher
                 "458d046151ad179c85429ed7420ffb1eaf6ddf85",
                 "efc0e34ede4e3204eaefb84a00e55e8c86634382"
         };
 
         public static String[] getLibraries(String[] old) {
-            if (old == null || old.length < 5) {
-                // <= 1.4.7
+            if (old == null) {
+                // Should never happen...
+                return old;
+            }
+            if (old.length < 4) {
+                // <= 1.4.5
                 return libraries;
+            }
+            if (old.length < 5) {
+                // <= 1.4.7
+                return libraries147;
             }
             // 1.5.x
             libraries15x[4] = old[4];
@@ -88,9 +114,17 @@ public class CoreFMLLibrariesTransformer extends MiniTransformer {
         }
 
         public static String[] getChecksums(String[] old) {
-            if (old == null || old.length < 5) {
-                // <= 1.4.7
+            if (old == null) {
+                // Should never happen...
+                return old;
+            }
+            if (old.length < 4) {
+                // <= 1.4.5
                 return checksums;
+            }
+            if (old.length < 5) {
+                // <= 1.4.7
+                return checksums147;
             }
             // 1.5.x
             checksums15x[4] = old[4];
